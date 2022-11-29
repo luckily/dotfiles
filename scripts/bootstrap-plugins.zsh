2641bf91b9249d62270651f3b55355dfbd4535bf
joelzhong <luckily.zhong@gmail.com>
2022-11-30 02:52:26 +0800

    fixup

--- scripts/bootstrap-plugins.zsh
+++ scripts/bootstrap-plugins.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 # [[ -e $HOME/.dotfiles/configs/.exports ]] && emulate sh -c "$HOME/.dotfiles/configs/.exports"
 # [[ -e $HOME/.dotfiles/configs/.functions ]] && emulate sh -c "$HOME/.dotfiles/configs/.functions"
-source $HOME/.dotfiles/configs/.exports
-source $HOME/.dotfiles/configs/.functions
-
+# source $HOME/.dotfiles/configs/.exports
+# source $HOME/.dotfiles/configs/.functions
+source $HOME/.dotfiles/configs/.zshrc
 
 # Install oh-my-zsh.
 # @see https://ohmyz.sh/#install
@@ -38,6 +38,7 @@ success "Install zsh-plugins successful."
 [[ -z $(command -v asdf) ]] && \
     info "Start to install asdf." && \
     brew install asdf && \
+    echo "source $HOME/.dotfiles/.homebrew/opt/asdf/libexec/asdf/sh" >> "$HOME/.dotfiles/configs/.zshrc" && \
     success "Install asdf successful."
 
 
@@ -46,7 +47,10 @@ success "Install zsh-plugins successful."
 [[ -z $(command -v sdk) ]] && \
     info "Start to install sdkman." && \
     curl -s "https://get.sdkman.io" | zsh && \
+    echo "source $HOME/.sdkman/bin/sdkman-init.sh" >> "$HOME/.dotfiles/configs/.zshrc" && \
     success "Install sdkman successful."
+
+source $HOME/.dotfiles/configs/.zshrc
 
 success "Bootstrap plugins successful."
 exit 0
